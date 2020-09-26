cd1c12f33b0dd3cd41d65704f43dabe88bc19e04
15870033509 <132010042@qq.com>
2020-09-26 23:50:19 +0800

    添加样式

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,22 @@
     <router-view/>
   </div>
 </template>
-
+<script lang="ts">
+// 1. 编辑器给的类型提示
+// 2. typescirpt 编译期间的类型提示
+import Vue from 'vue'
+export default Vue.extend({
+  data () {
+    return {
+      a: 1
+    }
+  },
+  methods: {
+    test () {
+      this.a.aaa()
+    }
+  }
+})
+</script>
 <style lang="scss" scoped>
 </style>
